c80b7fab721a6a5a8999187e3501339ecd5db794
Marina-yoya <64007447+Marina-yoya@users.noreply.github.com>
2023-10-31 14:56:17 +0000

    registration with unique email

--- laravel-api/app/Http/Controllers/Auth/RegisterController.php
+++ laravel-api/app/Http/Controllers/Auth/RegisterController.php
@@ -9,23 +9,51 @@ use App\Models\User;
 class RegisterController extends Controller
 {
    
-    public function register(Request $request)
-    {
+    // public function register(Request $request)
+    // {
         
-        $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
+    //     $request->validate([
+    //         'name' => 'required|string',
+    //         'email' => 'required|email|unique:users',
+    //         'password' => 'required|string|min:6',
+    //     ]);
 
         
-        $user = new User();
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        $user->password = bcrypt($request->input('password'));
-        $user->save();
+    //     $user = new User();
+    //     $user->name = $request->input('name');
+    //     $user->email = $request->input('email');
+    //     $user->password = bcrypt($request->input('password'));
+    //     $user->save();
 
       
-        return response()->json(['message' => 'User registered successfully'], 201);
+    //     return response()->json(['message' => 'User registered successfully'], 201);
+    // }
+
+//
+
+public function register(Request $request)
+{
+    $request->validate([
+        'name' => 'required|string',
+        'email' => 'required|email',
+        'password' => 'required|string|min:6',
+    ]);
+
+    $user = User::where('email', $request->input('email'))->first();
+
+    if ($user) {
+        return response()->json(['message' => 'Email already in use'], 422);
     }
+
+    $newUser = User::create([
+        'name' => $request->input('name'),
+        'email' => $request->input('email'),
+        'password' => bcrypt($request->input('password')),
+    ]);
+
+    return response()->json(['message' => 'User registered successfully'], 201);
+}
+
+
+
 }
